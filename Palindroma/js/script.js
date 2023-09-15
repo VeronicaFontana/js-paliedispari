059a18c1721350ffb483ae052433012a932e802a
VeronicaFontana <fontana.veronica98@gmail.com>
2023-09-15 17:31:23 +0200

    [ADD] Function palindroma

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,23 +1,26 @@
-const parola = prompt("Inserisci una parola");
+const parolaUtente = prompt("Inserisci una parola");
 
-const parolaArr = parola.split("");
-const primaParte = parolaArr.slice(0, (parola.length / 2));
-let secondaParte;
+palindroma(parolaUtente);
 
-if(parola.length % 2 === 0){
-  secondaParte = parolaArr.slice((parola.length / 2), parola.length);
-}else{
-  secondaParte = parolaArr.slice((parola.length / 2) + 1, parola.length);
+function palindroma(parola){
+  const parolaArr = parola.split("");
+  const primaParte = parolaArr.slice(0, (parola.length / 2));
+  let secondaParte;
+  
+  if(parola.length % 2 === 0){
+    secondaParte = parolaArr.slice((parola.length / 2), parola.length);
+  }else{
+    secondaParte = parolaArr.slice((parola.length / 2) + 1, parola.length);
+  }
+  
+  const reverse = secondaParte.reverse()
+  
+  const primaParola = primaParte.join(" ");
+  const secondaParola = reverse.join(" ");
+  
+  if(primaParola === secondaParola){
+    console.log("è palindroma")
+  }else{
+    console.log("non è palindroma")
+  }
 }
-
-const reverse = secondaParte.reverse()
-
-const primaParola = primaParte.join(" ");
-const secondaParola = reverse.join(" ");
-
-if(primaParola === secondaParola){
-  console.log("è palindroma")
-}else{
-  console.log("non è palindroma")
-}
-
