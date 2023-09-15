4c3c91fa925088efa8e94f0189b78a101b637dfa
VeronicaFontana <fontana.veronica98@gmail.com>
2023-09-15 16:23:27 +0200

    [FIX]Tolto ciclo, add passaggio array a stringhe

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -8,23 +8,27 @@ if (parola.length % 2 === 0){
   
   const reverse = secondaParte.reverse()
 
-  for(i = 0; i < (parola.length / 2); i++)
-    if(primaParte[i] === reverse[i]){
-      console.log("è palindroma")
-    }else{
-      console.log("non è palindroma")
-    }
+  const primaParola = primaParte.join(" ");
+  const secondaParola = reverse.join(" ");
+
+  if(primaParola === secondaParola){
+    console.log("è palindroma")
+  }else{
+    console.log("non è palindroma")
+  }
 }else{
   const primaParte = parolaArr.slice(0, (parola.length / 2));
   const secondaParte = parolaArr.slice((parola.length / 2) + 1, parola.length);
 
   const reverse = secondaParte.reverse()
 
-  for(i = 0; i < (parola.length / 2) - 1; i++)
-    if(primaParte[i] === reverse[i]){
-      console.log("è palindroma")
-    }else{
-      console.log("non è palindroma")
-    }
+  const primaParola = primaParte.join(" ");
+  const secondaParola = reverse.join(" ");
+
+  if(primaParola === secondaParola){
+    console.log("è palindroma")
+  }else{
+    console.log("non è palindroma")
+  }
 }
 
--- README.md
+++ README.md
@@ -5,6 +5,19 @@
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 
+Passaggi:
+1. Chiedo all'utente di inserire una parola
+2. Trasformo in array la parola
+3. Se la quantità dei caratteri è pari, divido la parola in due parti
+4. La seconda partela giro con reverse
+5. Trasformo le due parti in due stringhe e le confronto
+7. Se la quantità dei caratteri è dispari, divido sempre in due parti ma per la seconda tengo come punto di partenza la lettere successiva a quella centrale (che nelle palindrome rimane fissa)
+8. Come prima, ruoto con reverse la seconda parte, le trasformo in stringhe e le confronto
+
+
+
+
+
 **Pari e Dispari**
 
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
