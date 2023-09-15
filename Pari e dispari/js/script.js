06526fdfbdb25d171a313d716c9622b632cd5e24
VeronicaFontana <fontana.veronica98@gmail.com>
2023-09-15 17:41:42 +0200

    [ADD] Odd or even function

--- Pari e dispari/js/script.js
+++ Pari e dispari/js/script.js
@@ -1,9 +1,8 @@
-const pariDispari = prompt("Scrivi pari o dispari");
+const parola = prompt("Scrivi pari o dispari");
 const numero = parseInt(prompt("Scrivi un numero da 1 a 5"));
 
-console.log(pariDispari)
 
-if(pariDispari !== "pari" && pariDispari !== "dispari"){
+if(parola !== "pari" && pariDispari !== "dispari"){
   console.log("Devi scrivere pari o dispari");
 }
 
@@ -13,7 +12,6 @@ if(numero < 1 || numero > 5){
 
 const myRandom = randomizer(1, 5);
 console.log(myRandom);
-
 
 /**
  * 
@@ -25,17 +23,21 @@ function randomizer(min, max){
   return random;
 }
 
-const somma = myRandom + numero;
+const sum = myRandom + numero;
+parDis(sum, parola);
 
-if(somma % 2 == 0 && pariDispari === "pari"){
-  console.log("La somma è " + somma + " ed è pari. Vince il giocatore!");
-}else if(somma % 2 == 0 && pariDispari === "dispari"){
-  console.log("La somma è " + somma + " ed è dispari. Vince il giocatore!");
-}else if(somma % 2 == 0 && pariDispari === "dispari"){
-  console.log("La somma è " + somma + " ed è pari. Vince il computer!");
-}else{
-  console.log("La somma è " + somma + " ed è dispari. Vince il computer!");
+function parDis(somma, pariDispari){
+  if(somma % 2 == 0 && pariDispari === "pari"){
+    console.log("La somma è " + somma + " ed è pari. Vince il giocatore!");
+  }else if(somma % 2 == 0 && pariDispari === "dispari"){
+    console.log("La somma è " + somma + " ed è dispari. Vince il giocatore!");
+  }else if(somma % 2 == 0 && pariDispari === "dispari"){
+    console.log("La somma è " + somma + " ed è pari. Vince il computer!");
+  }else{
+    console.log("La somma è " + somma + " ed è dispari. Vince il computer!");
+  }
 }
+
 
 
 
