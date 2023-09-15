9cc547bf23a60b71892b6e73700c4f9993394982
VeronicaFontana <fontana.veronica98@gmail.com>
2023-09-15 16:50:53 +0200

    [FIX] Tolte ripetizioni

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,34 +1,23 @@
 const parola = prompt("Inserisci una parola");
+
 const parolaArr = parola.split("");
-console.log(parolaArr);
+const primaParte = parolaArr.slice(0, (parola.length / 2));
+let secondaParte;
 
-if (parola.length % 2 === 0){
-  const primaParte = parolaArr.slice(0, (parola.length / 2));
-  const secondaParte = parolaArr.slice((parola.length / 2), parola.length);
-  
-  const reverse = secondaParte.reverse()
-
-  const primaParola = primaParte.join(" ");
-  const secondaParola = reverse.join(" ");
-
-  if(primaParola === secondaParola){
-    console.log("è palindroma")
-  }else{
-    console.log("non è palindroma")
-  }
+if(parola.length % 2 === 0){
+  secondaParte = parolaArr.slice((parola.length / 2), parola.length);
 }else{
-  const primaParte = parolaArr.slice(0, (parola.length / 2));
-  const secondaParte = parolaArr.slice((parola.length / 2) + 1, parola.length);
+  secondaParte = parolaArr.slice((parola.length / 2) + 1, parola.length);
+}
 
-  const reverse = secondaParte.reverse()
+const reverse = secondaParte.reverse()
 
-  const primaParola = primaParte.join(" ");
-  const secondaParola = reverse.join(" ");
+const primaParola = primaParte.join(" ");
+const secondaParola = reverse.join(" ");
 
-  if(primaParola === secondaParola){
-    console.log("è palindroma")
-  }else{
-    console.log("non è palindroma")
-  }
+if(primaParola === secondaParola){
+  console.log("è palindroma")
+}else{
+  console.log("non è palindroma")
 }
 
